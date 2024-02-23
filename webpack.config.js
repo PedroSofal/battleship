14c26dfbf4588697162faeeffaf54484c1156f48
Pedro Sofal <pedrosofal@gmail.com>
2024-02-22 22:52:34 -0300

    webpack adicionado

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+    ]
+  },
+  devServer: {
+    static: './dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Battleship',
+      template: './src/index.html'
+    })
+  ]
+}
