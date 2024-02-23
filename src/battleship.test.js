461cb6dde1be3ed77cf8ca18eab2e583a7b03e1b
Pedro Sofal <pedrosofal@gmail.com>
2024-02-23 00:40:00 -0300

    primeiras implementações

---
+++ src/battleship.test.js
@@ -0,0 +1,2 @@
+import { testShip } from "./ship";
+
---
+++ src/ship.js
@@ -0,0 +1,32 @@
+class Ship {
+  size = null;
+  hits = 0;
+  
+  hit() {
+    this.hits++;
+  }
+
+  isSunk() {
+    return this.hits >= this.size;
+  }
+}
+
+class Carrier extends Ship {
+  size = 5;
+}
+
+class Battleship extends Ship {
+  size = 4;
+}
+
+class Cruiser extends Ship {
+  size = 3;
+}
+
+class Submarine extends Ship {
+  size = 3;
+}
+
+class Destroyer extends Ship {
+  size = 2;
+}
