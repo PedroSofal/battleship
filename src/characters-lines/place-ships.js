9586ab54b91f0f469e6078bebf5b3f3422d0f6d3
Pedro Sofal <pedrosofal@gmail.com>
2024-03-17 23:57:05 -0300

    feat: added lines for all characters in Place Ships menu

--- src/characters-lines/place-ships.js
+++ src/characters-lines/place-ships.js
@@ -24,49 +24,61 @@ export default class PlaceShipLines {
 
   static placing = {
     yamato: [
-      "Este #SHIP reforçará nossa posição, mantendo o equilíbrio da batalha.",
-      "Este #SHIP cortará as esperanças do inimigo como uma lâmina afiada.",
+      "O #SHIP reforçará nossa posição defensiva, mantendo o equilíbrio da batalha.",
+      "Cortaremos as esperanças do inimigo como uma lâmina afiada.",
+      "Tripulação do #SHIP, lembrem-se do nosso código de conduta e honrem a nossa pátria.",
+      "No silêncio antes da tempestade, encontro a calma interior e vejo com clareza as fraquezas do inimigo.",
     ],
     olafsson: [
       "Com este #SHIP, faremos tremer as águas e imporemos respeito.",
       "Nossas embarcações esmagarão o inimigo e nos devolverá a glória",
-      "O rugido do mar ecoa em meu peito como um trovão. Cada onda é um convite para a glória, e cada navio é uma extensão da bravura do nosso povo.",
+      "O rugido do mar ecoa como um trovão. Cada onda é um convite para a glória, e cada navio é uma extensão da bravura do nosso povo.",
+      "O #SHIP será como uma tempestade intransponível em alto mar.",
     ],
     violeta: [
-      "Este #SHIP será nossa arma secreta.",
-      "Posicionado estrategicamente, este #SHIP nos permitirá surpreender e desmantelar as forças inimigas.",
+      "O #SHIP será nossa arma secreta.",
+      "Com a estratégia ofensiva adequada, este #SHIP nos permitirá surpreender e desmantelar as forças inimigas rapidamente.",
+      "Sorrateiro como uma serpente, letal como um tubarão.",
+      "Emergiremos das sombras das ondas para mandá-los às profundezas do oceano.",
     ],
     rackham: [
-      "Este #SHIP será a chave para a conquista destas águas.",
+      "O #SHIP será a chave para a conquista destas águas.",
+      "Bons ventos e mares tranquilos, marujos.",
+      "Homens do #SHIP, será uma honra lutar ao seu lado.",
+      "Levantem as bandeiras, #SHIP!",
     ],
     nimitz: [
-      "Este #SHIP aqui ajudará a fortalecerá nossa linha de defesa estratégica.",
+      "Estou enviando o #SHIP para reforçar nossa frente de ataque.",
       "Com este #SHIP, anularemos as investidas inimigas e garantiremos a supremacia naval de nossa nação.",
+      "Todos apostos no convés!",
+      "#SHIP, prossiga 3 milhas náuticas a leste para sua posição de combate.",
     ],
     noronha: [
       "Este #SHIP será a chave para nossa tática de combate.",
-      "A criatividade sempre foi o nosso forte. O inimigo nunca esperaria encontrar nosso #SHIP aqui.",
+      "A criatividade sempre foi o nosso forte. Nosso #SHIP vindo desta posição será um grande elemento surpresa.",
+      "Somos resistentes como um #SHIP, capazes de superar qualquer obstáculo.",
+      "#O SHIP defenderá o perímetro.",
     ]
   }
 
   static reset = {
     yamato: [
-      "Reset Yamato",
+      "A busca pela mais precisa solução tática nosa leva aprimorar nossos planos.",
     ],
     olafsson: [
-      "Reset Olafsson",
+      "As mudanças das marés demandam adaptações em nossas táticas.",
     ],
     violeta: [
-      "Reset Violeta",
+      "Nada como a arte da ludibriação. O inimigo pensa que não tínhamos ciência de suas espionagens. Traçaremos o real plano de ação agora.",
     ],
     rackham: [
-      "Reset Rackham",
+      "Ótimo, alguém derramou bebida no mapa e agora precisaremos elaborar o plano novamente. Favor enviarem a equipe de limpeza para cá.",
     ],
     nimitz: [
-      "Reset Nimitz",
+      "Marinheiros, nosso planejamento sofrerá modificações.",
     ],
     noronha: [
-      "Reset Noronha",
+      "As novas condições climáticas demandam quer o plano B seja colocado em ação.",
     ]
   }
 
